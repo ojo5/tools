aa717c89d05987dfeeae3d013a22ca9465a32b40
ojo5 <109613345+ojo5@users.noreply.github.com>
2024-08-17 13:24:35 +0200

    Add files via upload

---
+++ reverseString.py
@@ -0,0 +1,19 @@
+# Python program to demonstrate
+# command line arguments
+
+import sys
+
+def echo_the_void(arg):
+    print("String reversed:",arg[::-1]) 
+
+if __name__ == "__main__":
+    # Name of Python scrip:
+    print("\nYou are running:", sys.argv[0])
+ 
+    if len(sys.argv) > 2:
+        raise ValueError('More than 1 input string.')
+
+    else:
+        print("Original string:",sys.argv[1])
+        echo_the_void(sys.argv[1])
+
